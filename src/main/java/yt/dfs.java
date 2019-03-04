164cb4ca294720010b723d249cd8bfdafe83afc9
Administrator <Administrator@0-187>
2019-03-04 14:28:12 +0800

    4

---
+++ src/main/java/yt/dfs.java
@@ -0,0 +1,9 @@
+package yt;
+
+public class dfs {
+	
+	public static void main(String[] args) {
+		System.out.println("+++++)))))))))))))))____________");
+	}
+
+}
---
+++ src/test/java/yt/NewTest.java
@@ -0,0 +1,10 @@
+package yt;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("++++++++++++______________++++++++++++++");
+  }
+}
