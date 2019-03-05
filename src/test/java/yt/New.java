b144d062d2a0f5700258f52b78c03365062e4256
50how <48091483+50how@users.noreply.github.com>
2019-03-05 08:59:37 +0800

    Update New.java

--- src/test/java/yt/New.java
+++ src/test/java/yt/New.java
@@ -2,7 +2,7 @@ package yt;
 
 import org.testng.annotations.Test;
 
-public class NewTest {
+public class New {
   @Test
   public void f() {
 	  System.out.println("++++++++++++_______12333______++++++++++++++");
