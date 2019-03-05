2494ba3c0b08edf052986495c9dfd9dee9b8aae8
Administrator <Administrator@0-187>
2019-03-05 17:36:55 +0800

    all of this

--- src/test/java/yt/NewTest.java
+++ src/test/java/yt/NewTest.java
@@ -1,10 +1,22 @@
 package yt;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 public class NewTest {
   @Test
   public void f() {
 	  System.out.println("++++++++++++_____我______++++++++++++++");
+	  System.setProperty("phantomjs.binary.path", "./libs/phantomjs.exe");
+	  
+//	  WebDriver driver=new FirefoxDriver();
+	  WebDriver driver=new PhantomJSDriver();
+	  driver.get("http://www.baidu.com");
+	  System.out.println(driver.getTitle().toString());
+	  driver.quit();
   }
+  
+  
 }
---
+++ src/test/java/yt/NewTest2.java
@@ -0,0 +1,11 @@
+package yt;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+	  
+  }
+}
